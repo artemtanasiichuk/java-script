06f754e8352e1433796583cc88978269231bfbbf
artemtanasiichuk <aizensama2356@gmail.com>
2025-06-23 17:14:01 +0200

    d

---
+++ index.js
@@ -0,0 +1,46 @@
+
+let age = 15;
+console.log("Ваш вік:", age);
+
+
+let name = "artem";
+console.log("Ваше ім'я:", name);
+
+
+let isStudent = true;
+console.log("Чи ви студент:", isStudent);
+
+
+let myString = "Не помиляється той, хто нічого не робить.";
+console.log("Цитата:", myString);
+
+let myNumber = 42;
+myNumber += 10;
+console.log("myNumber після додавання 10:", myNumber);
+
+
+let myNull = null;
+console.log("Значення myNull:", myNull);
+
+
+let userName = prompt("Введіть своє ім’я:");
+alert("Ваше ім’я: " + userName);
+console.log("Тип змінної userName:", typeof userName);
+
+
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log("Результат підтвердження:", userAnswer);
+if (userAnswer) {
+    console.log("Користувач погодився покинути сторінку.");
+} else {
+    console.log("Користувач відмінив дію.");
+}
+
+
+alert("Увага! Дія може бути небезпечною!");
+let continueAction = confirm("Ви впевнені, що хочете продовжити?");
+if (continueAction) {
+    console.log("Користувач підтвердив дію.");
+} else {
+    console.log("Користувач скасував дію.");
+}
