81267f0937de5e7f16d4f804cc3240ef1f6d381e
artemtanasiichuk <aizensama2356@gmail.com>
2025-06-30 18:18:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,31 @@ console.log("Ваш вік:", age);
 
 
 let name = "artem";
+const artem= "artem"
+
+
 console.log("Ваше ім'я:", name);
 
 
 let isStudent = true;
+
+
 console.log("Чи ви студент:", isStudent);
 
 
 let myString = "Не помиляється той, хто нічого не робить.";
 console.log("Цитата:", myString);
 
-let myNumber = 42;
+let myNumber = 4;
 myNumber += 10;
+
+
 console.log("myNumber після додавання 10:", myNumber);
 
 
 let myNull = null;
+
+
 console.log("Значення myNull:", myNull);
 
 
@@ -29,6 +38,8 @@ console.log("Тип змінної userName:", typeof userName);
 
 
 let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+
+
 console.log("Результат підтвердження:", userAnswer);
 if (userAnswer) {
     console.log("Користувач погодився покинути сторінку.");
